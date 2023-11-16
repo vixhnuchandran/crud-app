ad8f4531d4329a3a367b5e5574340485e9583478
vixhnuchandran <vixhnuchandran@gmail.com>
2023-11-16 11:33:55 +0530

    feat: add web routes for Clerk authentication and CRUD operations
    - Implement ClerkExpressWithAuth for authentication middleware
    - Define routes for home, dashboard, about, create, update, delete, and error pages
    - Utilize multer for file upload handling in create and update routes

---
+++ routes/webRoutes.js
@@ -0,0 +1,38 @@
+const { Router } = require("express")
+const { ClerkExpressWithAuth } = require("@clerk/clerk-sdk-node")
+const services = require("../services/render")
+const clerk = require("../config/clerk")
+const multer = require("multer")
+
+const route = Router()
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
+
+route.get("/", ClerkExpressWithAuth({}), services.home)
+
+route.get("/dashboard", ClerkExpressWithAuth({}), services.dashboard)
+
+route.get("/about", ClerkExpressWithAuth({}), services.about)
+
+route.get("/dashboard/create/", ClerkExpressWithAuth({}), services.create)
+
+route.get("/dashboard/update/:sid", ClerkExpressWithAuth({}), services.update)
+
+route.post(
+  "/dashboard/create",
+  ClerkExpressWithAuth({}),
+  upload.single("file"),
+  services.createStudent
+)
+
+route.post(
+  "/dashboard/update/:sid",
+  ClerkExpressWithAuth({}),
+  upload.single("file"),
+  services.updateStudent
+)
+
+route.patch("/dashboard/delete/:sid", ClerkExpressWithAuth({}), services.delete)
+
+route.get("/*", services.error)
+module.exports = route
