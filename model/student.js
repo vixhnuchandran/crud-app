3e6c37f731c23683c669dbe90da122a08a7624e9
vixhnuchandran <vixhnuchandran@gmail.com>
2023-11-16 11:39:57 +0530

    feat: define Sequelize model for 'students_info' table
    - Define Sequelize model with attributes for student information
    - Set primary key, auto-increment, and data types for each attribute
    - Add default values for optional attributes

---
+++ model/student.js
@@ -0,0 +1,50 @@
+const { sequelize } = require("../config/database")
+const { DataTypes } = require("sequelize")
+
+const Students = sequelize.define(
+  "students_info",
+  {
+    s_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    s_firstname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    s_lastname: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    s_birthdate: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    s_contactno: {
+      type: DataTypes.STRING(10),
+      allowNull: false,
+    },
+    s_address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    s_image_url: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue: null,
+    },
+    isRemoved: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  },
+  { freezeTableName: true }
+)
+
+// Students.sync({ force: true }).then(() => {
+//   console.log(`Table created!`)
+// })
+
+module.exports = Students
