1b04117f82f07d8a94b6c790a1f419df881fee20
vixhnuchandran <vixhnuchandran@gmail.com>
2023-11-28 13:04:35 +0530

    feat: Implement API routes to work with MongoDB

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -54,17 +54,17 @@ route.get("/api/v1/students", async (req, res) => {
 
 route.get("/api/v1/students/:sid", async (req, res) => {
   const sid = req.params.sid
-  const data = await Students.findByPk(sid, {
-    where: { isRemoved: false },
-    attributes: [
-      ["s_id", "studentId"],
-      ["s_firstname", "firstname"],
-      ["s_lastname", "lastname"],
-      ["s_birthdate", "birthdate"],
-      ["s_emailid", "email"],
-      ["s_class", "class"],
-      ["s_image_url", "imageurl"],
-    ],
+  const data = await Students.findOne({
+    isRemoved: false,
+    s_id: sid,
+  }).select({
+    studentId: "$s_id",
+    firstname: "$s_firstname",
+    lastname: "$s_lastname",
+    birthdate: "$s_birthdate",
+    email: "$s_emailid",
+    class: "$s_class",
+    imageurl: "$s_image_url",
   })
 
   return res.status(200).json(data)
