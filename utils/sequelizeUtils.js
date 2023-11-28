afa7feacb91f5e201ee0b945d40f565ddf558222
vixhnuchandran <vixhnuchandran@gmail.com>
2023-11-28 12:37:58 +0530

    feat: Add Sequelize utility functions in utils/sequelize utils.js

---
+++ utils/sequelizeUtils.js
@@ -0,0 +1,131 @@
+const connectDB = require("../config/postgres")
+const Students = require("../models/postgres/student")
+const Marks = require("../models/postgres/marks")
+const Crudlogs = require("../models/postgres/crudlog")
+
+// Function to create new student data
+const createStudent = async newData => {
+  try {
+    const response = await Students.create(newData)
+    return response
+  } catch (error) {
+    console.error(error.message)
+    throw error
+  }
+}
+
+// Function to find student data
+const readStudent = {
+  // All student data
+  all: async () => {
+    try {
+      const response = (data = await Students.findAll({
+        where: {
+          isRemoved: false,
+        },
+        attributes: {
+          exclude: ["isRemoved"],
+        },
+        order: [["s_id", "ASC"]],
+      }))
+
+      return response
+    } catch (error) {
+      console.error(error.message)
+      throw error
+    }
+  },
+
+  // One student's data
+  one: async studentId => {
+    try {
+      const response = await Students.findOne({
+        where: { isRemoved: false, s_id: studentId },
+        include: [
+          {
+            model: Marks,
+            attributes: [
+              "maths",
+              "physics",
+              "chemistry",
+              "biology",
+              "language",
+            ],
+          },
+        ],
+        attributes: {
+          exclude: ["isRemoved"],
+        },
+      })
+      return response
+    } catch (error) {
+      console.error(error.message)
+      throw error
+    }
+  },
+}
+
+// Function to update a student data
+const updateStudent = async (studentId, studentData) => {
+  try {
+    const response = await Students.update(studentData, {
+      where: { isRemoved: false, s_id: studentId },
+    })
+    return response
+  } catch (error) {
+    console.error(error.message)
+    throw error
+  }
+}
+
+// Function to update(or soft delete) a student data
+const deleteStudent = async studentId => {
+  try {
+    const response = await Students.update(
+      { isRemoved: true },
+      {
+        where: { isRemoved: false, s_id: studentId },
+      }
+    )
+    return response
+  } catch (error) {
+    console.error(error.message)
+    throw error
+  }
+}
+module.exports = { createStudent, readStudent, updateStudent, deleteStudent }
+
+// await Students.findAll({
+//   where: { isRemoved: false },
+//   attributes: attributes || [
+//     ["s_id", "studentId"],
+//     ["s_firstname", "firstname"],
+//     ["s_lastname", "lastname"],
+//     ["s_birthdate", "birthdate"],
+//     ["s_emailid", "email"],
+//     ["s_class", "class"],
+//     ["s_image_url", "imageurl"],
+//   ],
+// })
+
+// const data = await StudentsM.findOne({
+//   where: { isRemoved: false, s_id: sid },
+//   include: [
+//     {
+//       model: Marks,
+//       attributes: [
+//         "maths",
+//         "physics",
+//         "chemistry",
+//         "biology",
+//         "language",
+//       ],
+//     },
+//   ],
+//   attributes: [
+//     ["s_id", "studentId"],
+//     ["s_firstname", "firstname"],
+//     ["s_lastname", "lastname"],
+//     ["s_image_url", "imageurl"],
+//   ],
+// })
