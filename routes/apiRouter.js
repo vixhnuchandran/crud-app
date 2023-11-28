110043bf1cbcff843208a1feba799179dbcc4378
vixhnuchandran <vixhnuchandran@gmail.com>
2023-11-28 12:54:18 +0530

    feat: Implement API routes to work with MongoDB

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,34 +1,54 @@
 const { Router } = require("express")
 const route = Router()
-const Students = require("../model/student")
-const Crudlogs = require("../model/crudlog")
+const { Students } = require("../models/mongo/students")
+const Crudlogs = require("../models/postgres/crudlog")
 const { Op } = require("sequelize")
+const mangoose = require("mongoose")
+const { readData } = require("../utils/mongooseUtils")
 
 route.get("/api/v1/students", async (req, res) => {
   const search = req.query.search
-  console.log(search)
-  const data = await Students.findAll({
-    where: {
+  let data
+
+  if (search) {
+    data = await Students.find({
       isRemoved: false,
-
-      [Op.or]: [
-        { s_firstname: { [Op.iLike]: `%${search}%` } },
-        { s_lastname: { [Op.iLike]: `%${search}%` } },
-        { s_emailid: { [Op.iLike]: `%${search}%` } },
+      $or: [
+        { s_firstname: { $regex: new RegExp(search, "i") } },
+        { s_lastname: { $regex: new RegExp(search, "i") } },
+        { s_emailid: { $regex: new RegExp(search, "i") } },
       ],
-    },
-    attributes: [
-      ["s_id", "studentId"],
-      ["s_firstname", "firstname"],
-      ["s_lastname", "lastname"],
-      ["s_birthdate", "birthdate"],
-      ["s_emailid", "email"],
-      ["s_class", "class"],
-      ["s_image_url", "imageurl"],
-    ],
-    order: [["s_id", "ASC"]],
-  })
-
+    }).select({
+      studentId: "$s_id",
+      firstname: "$s_firstname",
+      lastname: "$s_lastname",
+      birthdate: "$s_birthdate",
+      email: "$s_emailid",
+      class: "$s_class",
+      imageurl: "$s_image_url",
+    })
+    console.log(data)
+  } else {
+    data = await Students.findAll({
+      //     where: {
+      //       isRemoved: false,
+      //      },
+      attributes: {
+        exclude: [
+          "s_image_url",
+          "isRemoved",
+          //   ["s_id", "studentId"],
+          //   ["s_firstname", "firstname"],
+          //   ["s_lastname", "lastname"],
+          //   ["s_birthdate", "birthdate"],
+          //   ["s_emailid", "email"],
+          //   ["s_class", "class"],
+          //   ["s_image_url", "imageurl"],
+        ],
+      },
+      order: [["s_id", "ASC"]],
+    })
+  }
   return res.status(200).json(data)
 })
 
@@ -50,17 +70,17 @@ route.get("/api/v1/students/:sid", async (req, res) => {
   return res.status(200).json(data)
 })
 
-route.get("/api/v1/logs", async (req, res) => {
-  const data = await Crudlogs.findAll({
-    attributes: [
-      ["user_id", "User ID"],
-      ["action_type", "Action"],
-      ["target_student_id", "Target ID"],
-      ["timestamp", "Timestamp"],
-    ],
-  })
+// route.get("/api/v1/logs", async (req, res) => {
+//   const data = await Crudlogs.findAll({
+//     attributes: [
+//       ["user_id", "User ID"],
+//       ["action_type", "Action"],
+//       ["target_student_id", "Target ID"],
+//       ["timestamp", "Timestamp"],
+//     ],
+//   })
 
-  return res.status(200).json(data)
-})
+//   return res.status(200).json(data)
+// })
 
 module.exports = route
