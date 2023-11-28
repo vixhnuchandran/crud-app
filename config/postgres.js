d885b2799267b058de81ea7d26f19f2c7cecaa6e
vixhnuchandran <vixhnuchandran@gmail.com>
2023-11-28 12:36:45 +0530

    chore: Update PostgreSQL configuration in config/postgres.js

---
+++ config/postgres.js
@@ -0,0 +1,18 @@
+const { Sequelize } = require("sequelize")
+require("pg")
+require("dotenv").config()
+
+const sequelize = new Sequelize(process.env.POSTGRES_URL + "?sslmode=require", {
+  dialect: "postgres",
+})
+
+const connectDB = async function () {
+  try {
+    await sequelize.authenticate()
+    console.log(`Connection has been established successfully`)
+  } catch (error) {
+    console.error(`Unable to connect to the database, ${error.message}`)
+  }
+}
+
+module.exports = { connectDB, sequelize }
