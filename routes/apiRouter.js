97d6fea5c73c978b2db686379bff0b23b19cb8a0
vixhnuchandran <vixhnuchandran@gmail.com>
2023-12-04 12:45:10 +0530

    Remove unnecessary console logs

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -5,30 +5,16 @@ const { Students } = require("../models/mongo/students")
 route.get("/api/v1/students", async (req, res) => {
   const search = req.query.search
   let data
-
-  if (search) {
-    data = await Students.find({
-      isRemoved: false,
-      $or: [
-        { s_firstname: { $regex: new RegExp(search, "i") } },
-        { s_lastname: { $regex: new RegExp(search, "i") } },
-        { s_emailid: { $regex: new RegExp(search, "i") } },
-      ],
-    }).select({
-      studentId: "$s_id",
-      firstname: "$s_firstname",
-      lastname: "$s_lastname",
-      birthdate: "$s_birthdate",
-      email: "$s_emailid",
-      class: "$s_class",
-      imageurl: "$s_image_url",
-    })
-    console.log(data)
-  } else {
-    data = await Students.find({
-      isRemoved: false,
-    })
-      .select({
+  try {
+    if (search) {
+      data = await Students.find({
+        isRemoved: false,
+        $or: [
+          { s_firstname: { $regex: new RegExp(search, "i") } },
+          { s_lastname: { $regex: new RegExp(search, "i") } },
+          { s_emailid: { $regex: new RegExp(search, "i") } },
+        ],
+      }).select({
         studentId: "$s_id",
         firstname: "$s_firstname",
         lastname: "$s_lastname",
@@ -37,9 +23,27 @@ route.get("/api/v1/students", async (req, res) => {
         class: "$s_class",
         imageurl: "$s_image_url",
       })
-      .sort({ s_id: 1 })
-  }
-  return res.status(200).json(data)
+    } else {
+      data = await Students.find({
+        isRemoved: false,
+      })
+        .select({
+          studentId: "$s_id",
+          firstname: "$s_firstname",
+          lastname: "$s_lastname",
+          birthdate: "$s_birthdate",
+          email: "$s_emailid",
+          class: "$s_class",
+          imageurl: "$s_image_url",
+        })
+        .sort({ s_id: 1 })
+    }
+    if (data.length > 0) {
+      return res.status(200).json(data)
+    } else {
+      return res.json({ message: "No matching records found" })
+    }
+  } catch (error) {}
 })
 
 route.get("/api/v1/students/:sid", async (req, res) => {
@@ -59,18 +63,5 @@ route.get("/api/v1/students/:sid", async (req, res) => {
 
   return res.status(200).json(data)
 })
-
-// route.get("/api/v1/logs", async (req, res) => {
-//   const data = await Crudlogs.findAll({
-//     attributes: [
-//       ["user_id", "User ID"],
-//       ["action_type", "Action"],
-//       ["target_student_id", "Target ID"],
-//       ["timestamp", "Timestamp"],
-//     ],
-//   })
-
-//   return res.status(200).json(data)
-// })
 
 module.exports = route
